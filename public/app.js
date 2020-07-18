3e10433a976b44a02d0ab0e3ddac39359b252eae
Glitch (naf-boilerplate) <none>
2020-07-18 19:57:24 +0000

    first commit

---
+++ public/app.js
@@ -0,0 +1,28 @@
+ // Called by Networked-Aframe when connected to server
+function onConnect () {
+  
+  console.log("onConnect", new Date());
+  
+  const socket = NAF.connection.adapter.socket
+  
+  
+  //NAF.login('admin','password')
+  NAF.login = (name,pw)=>{
+    socket.emit('login' , {name:name , pw:pw})
+  }
+  
+  NAF.addUser = ( data , cb  )=>{
+    socket.emit('add-user' , {key: data.key ,name:data.name , pw:data.pw} , cb )
+  }
+  
+  
+
+  socket.on('login-results' , results=>{
+    
+    console.log('success: ' , results.success)
+    console.log('name: ' , results.name)
+    
+  })
+  
+}
+
